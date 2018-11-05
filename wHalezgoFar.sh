eb564d2919d96f5c38a279d5d4d53b6861f4aa18
daogster <26748523+daogster@users.noreply.github.com>
2018-11-05 17:28:30 -0600

    Update wHalezgoFar.sh

--- wHalezgoFar.sh
+++ wHalezgoFar.sh
@@ -2,11 +2,10 @@
 # update install run & report
  sudo apt-get update
  sudo apt-get install cmake build-essential libboost-all-dev
+ /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
  brew install hwloc libmicrohttpd gcc openssl cmake
  cmake . -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
- make install
- git clone https://github.com/daogster/xmr-stak.git
- cd xmr-stak
- ls
- 
-
+ git clone https://github.com/Bendr0id/xmrigCC.git    
+ cd xmrigCC && mkdir build && cd build    
+ cmake ..    
+ make
