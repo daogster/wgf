d4e53c8b7b2f5d1573886eed2408c78f4cf88116
daogster <26748523+daogster@users.noreply.github.com>
2018-11-06 18:39:31 -0600

    Delete try.sh

--- try.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Update install run & report
- sudo apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
- git clone https://github.com/fireice-uk/xmr-stak.git
- mkdir xmr-stak/build
- cd xmr-stak/build
- cmake ..
- make install
- 
