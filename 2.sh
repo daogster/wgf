ebd0d4c94b56edaa0063e721b50f04f972ac08f4
daogster <26748523+daogster@users.noreply.github.com>
2018-11-06 20:18:43 -0600

    Add files via upload
    config and command

--- 2.sh
+++ 2.sh
@@ -1,14 +1,42 @@
-#!/bin/bash
-# Update install run & report
- sudo apt update
- sudo apt install gcc-5 g++-5 make
- sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 1 --slave /usr/bin/g++ g++ /usr/bin/g++-5
- curl -L http://www.cmake.org/files/v3.4/cmake-3.4.1.tar.gz | tar -xvzf - -C /tmp/
- cd /tmp/cmake-3.4.1/ && ./configure && make && sudo make install && cd -
- sudo update-alternatives --install /usr/bin/cmake cmake /usr/local/bin/cmake 1 --force
- sudo apt install libmicrohttpd-dev libssl-dev libhwloc-dev
- git clone https://github.com/fireice-uk/xmr-stak.git
- mkdir xmr-stak/build
- cd xmr-stak/build
- cmake ..
- make install
+command line alloy linux below:
+./xmrig --donate-level 0 --api-port 5661 --max-cpu-usage 100 -o alloy.ingest.cryptoknight.cc -u AAz3qdpLqoV5dWyqxYhzST2FUqCtovpRzGho7TCATGxr9Mj42ynVEabd5qpg71uXFYDiaoTKLpsiZDAbbxqFqHewKmuKR3Z -p x --variant xao -k
+
+confgi.json below:
+{
+    "algo": "cryptonight",
+    "api": {
+        "port": 5661,
+        "access-token": null,
+        "worker-id": null,
+        "ipv6": false,
+        "restricted": true
+    },
+    "av": 0,
+    "background": false,
+    "colors": true,
+    "cpu-affinity": null,
+    "cpu-priority": null,
+    "donate-level": 0,
+    "huge-pages": true,
+    "hw-aes": null,
+    "log-file": null,
+    "max-cpu-usage": 100,
+    "pools": [
+        {
+            "url": "alloy.ingest.cryptoknight.cc",
+            "user": "AAz3qdpLqoV5dWyqxYhzST2FUqCtovpRzGho7TCATGxr9Mj42ynVEabd5qpg71uXFYDiaoTKLpsiZDAbbxqFqHewKmuKR3Z",
+            "pass": "x",
+            "keepalive": true,
+            "nicehash": false,
+            "variant": "xao",
+            "tls": false,
+            "tls-fingerprint": null
+        }
+    ],
+    "print-time": 60,
+    "retries": 5,
+    "retry-pause": 5,
+    "safe": false,
+    "syslog": false,
+    "threads": null
+}
